06c61f311302012f69aaba9416f48061062311ab
dyf <david.feng@alleninstitute.org>
2022-01-21 21:21:08 -0800

    writing session tables working

--- pgaf/ingest.py
+++ pgaf/ingest.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 import sqla_schema as sch
+import json
 
 
 manifest = '/allen/scratch/aindtemp/david.feng/epc/manifest.json'
@@ -17,15 +18,20 @@ cache.get_units()
 for idx,row in sessions.head(3).iterrows():
     cache.get_session_data(idx)
 
+with open('SECRETS','r') as f:
+    conn_info = json.load(f)
 
-engine = create_engine('sqlite://')
+conn_string = f"postgresql://{conn_info['user']}:{conn_info['password']}@{conn_info['host']}:5432/{conn_info['dbname']}"
+
+engine = create_engine(conn_string)
+sch.Base.metadata.drop_all(engine)
 sch.Base.metadata.create_all(engine)
 
-sessions.to_sql('session', engine, if_exists='replace')
+sessions.drop(columns=['ecephys_structure_acronyms']).to_sql('session', engine, if_exists='append')
 
-with Session(engine) as sess:
+with Session(engine) as session:
     q = select(sch.Session).where(sch.Session.sex == 'M')
-    for row in sess.execute(q):
-        print(dict(row))
+    for r in session.execute(q):
+        print(r[0])
     
 
